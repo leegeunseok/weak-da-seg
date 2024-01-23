6597288c26641e6de87bb16769ff4bd954cc96fa
KDW1998 <rkdwodbs1@gmail.com>
2024-01-23 19:37:21 +0900

    add crack dataset

--- daweak/dataset/__init__.py
+++ daweak/dataset/__init__.py
@@ -5,11 +5,23 @@
 from .gta5 import GTA5Segmentation
 from .cityscapes import CityscapesSegmentation
 from .synthia import SYNTHIADataSet
+from .gaps_s import GAPSSSegmentation
+from .gaps_t import GAPSTSegmentation
+from .crack500_s import CRACK500SSegmentation
+from .crack500_t import CRACK500TSegmentation
+from .rissbilder_s import RissbilderSSegmentation
+from .rissbilder_t import RissbilderTSegmentation
 
 datasets = {
     'gta5': GTA5Segmentation,
     'cityscapes': CityscapesSegmentation,
     'synthia': SYNTHIADataSet,
+    'gaps_s': GAPSSSegmentation,
+    'gaps_t': GAPSTSegmentation,
+    'crack500_s': CRACK500SSegmentation,
+    'crack500_t': CRACK500TSegmentation,
+    'rissbilder_s': RissbilderSSegmentation,
+    'rissbilder_t': RissbilderTSegmentation
 }
 
 
